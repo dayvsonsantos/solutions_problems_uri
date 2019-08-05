a4658ba085885aae596722100176db30b675ca6b
dayvsonsantos <dayvsondossantossilva@gmail.com>
2019-08-05 18:23:18 -0400

    Add all files

---
+++ 1001/1001.py
@@ -0,0 +1,9 @@
+# sum of two interges numbers
+
+# while True:
+#    try:
+number_1 = int(input())
+number_2 = int(input())
+print("X =", number_1 + number_2)
+#    except EOFError:
+#        break
---
+++ 1018/1018.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+// (int)  9.0 - 4.0
+    int v[] = {100, 50, 20, 10, 5, 2};
+
+    scanf("%i", &num);
+}
