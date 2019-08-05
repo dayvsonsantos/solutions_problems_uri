83eff29331eb566176bf2c3e196fb92d5c497443
dayvsonsantos <dayvsondossantossilva@gmail.com>
2019-08-05 19:00:26 -0400

    problem 1018

--- 1018/1018.c
+++ 1018/1018.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-
-// (int)  9.0 - 4.0
-    int v[] = {100, 50, 20, 10, 5, 2};
-
+int main(){
+    int cell[7] = {100, 50, 20, 10, 5, 2, 1};
+    int num;
+    int amount;
+    int i;
     scanf("%i", &num);
+    printf("%i\n", num);
+    for (i = 0; i < 7; i++){
+        amount = (int) num / cell[i];
+        num = (int) num % cell[i];
+        printf("%i nota(s) de R$ %i,00\n", amount, cell[i]);
+    }
+    return (0);
 }
