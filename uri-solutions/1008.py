1c1c5ed37ded5c9e146c3b116e0994178a944bee
Dayvson <ddss.snf19@uea.edu.br>
2020-12-21 20:07:50 +0000

    Traning with esy problems

---
+++ uri-solutions/1008.py
@@ -0,0 +1,8 @@
+func_numb = int(input())
+time_numb = int(input())
+time_value = float(input())
+
+salary = time_numb * time_value
+
+print("NUMBER =", func_numb)
+print("SALARY = U$ %.2f" %salary)
---
+++ uri-solutions/1009.py
@@ -0,0 +1,7 @@
+name_empl = input()
+static_salary = float(input())
+comition_value = float(input())
+
+total = 0.15 * comition_value + static_salary
+
+print('TOTAL = R$ %.2f' %total)
